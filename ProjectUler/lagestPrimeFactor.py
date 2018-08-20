367fe87d3d77154e6b8ca2a7f9c0e0e01b51c288
nithin <nithinrithin@gmail.com>
2018-08-20 18:45:55 +0530

    find the largest prime factor for the number

---
+++ ProjectUler/lagestPrimeFactor.py
@@ -0,0 +1,24 @@
+"""
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+
+"""
+import time
+max = input("Please enter the value ");
+start_time = time.time()
+up_time = 0
+for num in range(1, int(max**.5)+1):
+    for i in range(2, int(num**.5)+1):
+        if ((num % i) == 0):
+            break
+    else:
+        if (max % num == 0):
+            print "======",num
+    up_time = time.time()-start_time
+    if (up_time > 20):
+        print "break++++++++++++",num
+        print up_time
+        exit(1)
+print "total time", up_time
